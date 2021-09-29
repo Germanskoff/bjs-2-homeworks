11a223535793c48402a875631718257e74443d41
Germanskov <germanskoff@gmail.com>
2021-09-29 21:50:26 +0300

    germanskov

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,11 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  let d = b**2-4*a*c;
+  let x;
+  if (d < 0) {arr = []}
+  else if (d = 0) {arr = [x = -b/(2*a)]}
+  else {arr = [(-b + Math.sqrt(d))/(2*a)]}; 
   return arr; // array
 }
 
